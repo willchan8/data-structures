5965424d0508cd8e056165e4935202431e493494
William Chan <wchan1992@gmail.com>
2019-04-10 18:13:00 -0700

    Implement to pass all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,30 @@
 var Queue = function() {
-  var someInstance = {};
+  var someInstance = {}; // the three functions below are added to the 'someInstance'
 
   // Use an object with numeric keys to store values
   var storage = {};
 
-  // Implement the methods below
+  var count = 0;
 
+  // Implement the methods below
   someInstance.enqueue = function(value) {
+    // Add to the back
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    // Remove from the front (remove smallest index)
+    var keys = Object.keys(storage);
+    var firstKey = keys[0];
+    var toBeDequeued = storage[firstKey];
+    delete storage[firstKey];
+    return toBeDequeued;
   };
 
   someInstance.size = function() {
+    // 'Length' of the object
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,28 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var count = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+  // Add to the top (becomes largest index)
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+  // Remove from the top (removes largest index)
+    var toBePopped = storage[count - 1];
+    if (count > 0){
+      delete storage[count - 1];
+      count--;
+    };
+    return toBePopped;
   };
 
   someInstance.size = function() {
+  // 'Length' of object
+    return count;
   };
 
   return someInstance;
