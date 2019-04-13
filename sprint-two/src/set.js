fce80fd951a9fe1d7c0df60218c9869b072ade9c
willchan8 <wchan1992@gmail.com>
2019-04-12 21:00:53 -0700

    Implement set to pass all tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,23 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  return this.storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  this.storage = this.storage.filter(function (element) {
+    return element !== item;
+  })
 };
 
 /*
